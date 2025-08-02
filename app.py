2b059e69920681a1c307f1e414580b2a8e99199f
GaryLore <garyperez1369@gmail.com>
2025-08-02 16:45:06 -0400

    Implementing Pieces to appear on board

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import chess
+import tensorflow as tf
+from tensorflow import keras
 from flask import Flask, render_template
+from ChessRun import *
 
 app = Flask(__name__)
-
+checkmate_model = keras.models.load_model("models/mate_model.keras")
+evaluation_model = keras.models.load_model("models/epoch_20.keras")
+board = chess.Board()
 
 @app.route('/')
 def index():
@@ -10,3 +16,7 @@ def index():
 @app.route('/board')
 def board():
     return render_template('board.html')
+
+@app.route('/move', methods=['POST'])
+def move():
+    pass

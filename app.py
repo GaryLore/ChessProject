c676e91781f673d134eef08c481eeff1025e0d05
GaryLore <garyperez1369@gmail.com>
2025-08-01 22:20:58 -0400

    Flask

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/board')
+def board():
+    return render_template('board.html')

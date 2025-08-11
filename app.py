81095abf53fd1a880f96752216c3348f0e263865
GaryLore <garyperez1369@gmail.com>
2025-08-10 20:32:16 -0400

    implemented sessions

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import chess
 import tensorflow as tf
 from tensorflow import keras
-from flask import Flask, render_template, jsonify, request, make_response, redirect, url_for
+from flask import Flask, render_template, jsonify, request, make_response, redirect, url_for, session
 from ChessRun import *
 
 app = Flask(__name__)
-game = chess.Board()
+app.secret_key = "2925942692912a3f983e48b1d5ed066915a4a9d2851846e0d2a150db199b7549"
+app.config.update(
+    SECRET_KEY=app.secret_key,   # Loaded from environment
+    SESSION_COOKIE_SECURE=True,                # Cookie sent only over HTTPS
+    SESSION_COOKIE_HTTPONLY=True,              # JS can't access cookie
+    SESSION_COOKIE_SAMESITE="Lax"              # Blocks cross-site requests
+)
 
 @app.route('/')
 def index():
@@ -13,6 +19,9 @@ def index():
 
 @app.route('/board')
 def board_page():
+    if "FEN" not in session:
+        session["FEN"] = chess.Board().fen()##assigns default fen
+
     return render_template('board.html')
 
 @app.route('/instructions')
@@ -21,17 +30,19 @@ def instructions():
 
 @app.route('/reset')
 def reset():
-    global game
-    game = chess.Board()
+    session["FEN"] = chess.Board().fen()##assigns default fen
     return redirect(url_for('board_page'))
 
 @app.route('/submit_move', methods=['POST'])
 def move():
+    game = initializeBoard()
     player_move = request.get_json()
     print(player_move['move'])
     print("White Turn to Move")
     if make_move(player_move['move'], game):
+        session["fen"] = game.fen()
         print("Success")
+        session["FEN"] = game.fen()
         return jsonify(success=True)
     else:
         print("illegal move")
@@ -39,7 +50,8 @@ def move():
 
 @app.route("/update_state")
 def get_board_state():
-
+    print("TESTING SESSION COOKIE:  " + session["FEN"])
+    game = initializeBoard()
     if game.is_game_over():
         info = game.outcome()
         print("Game termination : " + str(info.termination))
@@ -70,8 +82,13 @@ def get_board_state():
 
 @app.route('/ai_move')
 def ai_move():
+    game = initializeBoard()
     move = ai_select_move(game)
     print("ai move" + move)
     from_move = move[0:2]
     to_move = move[2:4]
+    session["FEN"] = game.fen()
     return jsonify(fromMove = from_move, toMove = to_move)
+
+def initializeBoard():
+    return chess.Board(session["FEN"])
